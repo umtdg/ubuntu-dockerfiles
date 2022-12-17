c22ef38a253c0808c13cb3c57d6b34bdaa277a3d
Umut Dag <umu7d9@gmail.com>
2022-12-17 13:38:20 +0300

    fix build and push errors
    Signed-off-by: Umut Dag <umu7d9@gmail.com>

--- build.sh
+++ build.sh
@@ -1,25 +1,88 @@
 #!/bin/bash
 
+declare -A versions=(
+    [16.04]="yes"
+    [18.04]="yes"
+    [20.04]="yes"
+    [22.04]="yes"
+)
+
 build() {
-    folder="$1"
-    dev="$2"
+    ver=$1
+    dev=$2
 
-    tag="umtdg/${folder}:latest"
-    dockerfile="${folder}/Dockerfile"
-    [[ "${dev}" == "yes" ]] && dockerfile="${dockerfile}.dev"
+    tag="umtdg/ubuntu:$ver"
+    dockerfile="$ver/Dockerfile"
+    if [ "$dev" == "yes" ]; then
+        tag="$tag-dev"
+        dockerfile="$dockerfile.dev"
+    fi
 
-    [[ -d "${folder}" ]] && echo "Found folder ${folder}" || { echo "Could not found ${folder}"; exit 1; }
-    [[ -f "${dockerfile}" ]] && echo "Found Dockerfile ${dockerfile}" || { echo "Could not found Dockerfile ${dockerfile}"; exit 1; }
+    [[ -d "$ver" ]] || { echo "Could not found $ver"; exit 1; }
+    [[ -f "$dockerfile" ]] || { echo "Could not found $dockerfile"; exit 1; }
 
-    echo "Building ${tag}"
-    docker build -t "${tag}" -f "${dockerfile}" "${folder}"
-
-    echo "Pushing ${tag}"
-    docker push "${tag}"
+    echo -e "Building $tag\n"
+    docker buildx build --file $dockerfile --tag $tag $ver
 }
 
-for distro in $(find . -type d -regextype posix-extended -regex '.*[0-9]{2}\.04' -exec basename {} \;)
-do
-    build "${distro}" "no"
-    build "${distro}" "yes"
+build_all() {
+    for key in "${!versions[@]}"; do
+        build $key no
+        echo
+
+        build $key yes
+        echo
+    done
+}
+
+push() {
+    tag="umtdg/ubuntu:$1"
+    [[ "$2" == "yes" ]] && tag="$tag-dev"
+
+    echo -e "Pushing $tag\n"
+    docker push $tag
+}
+
+push_all() {
+    for key in "${!versions[@]}"; do
+        push $key no
+        echo
+
+        push $key yes
+        echo
+    done
+}
+
+version=""
+do_dev="no"
+do_push="no"
+do_push_only="no"
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -d|--dev|--developer) do_dev="yes"; shift; ;;
+        -v|--ver|--versions) version="$2"; shift 2; ;;
+        -p|--push) do_push="yes"; shift; ;;
+        -P|--push-only) do_push_only="yes"; shift; ;;
+        *) shift; ;;
+    esac
 done
+
+if [ -z "$version" ]; then
+    if [ "$do_push_only" == "yes" ]; then
+        push_all
+    else
+        build_all
+        [[ "$do_push" == "yes" ]] && push_all
+    fi
+elif [ "${versions[$version]}" == "yes" ]; then
+    if [ "$do_push_only" == "yes" ]; then
+        push $version $do_dev
+    else
+        build $version $do_dev
+        [[ "$do_push" == "yes" ]] && push $version $do_dev
+    fi
+else
+    echo "Unknown version: $version"
+fi
+
