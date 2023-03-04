7d2ad5d6bc19785ded39f261ea516e132523a81c
Umut Dag <umut.dag@ulakhaberlesme.com.tr>
2023-03-04 13:24:56 +0300

    add iproute2 ping and dnsutils

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+docker="docker"
+if ! id -nG $USER | grep -qw docker; then
+    docker="sudo $docker"
+fi
+
 declare -A versions=(
     [16.04]="yes"
     [18.04]="yes"
@@ -21,9 +26,9 @@ build() {
         dockerfile="$dockerfile.dev"
     fi
 
-    if docker images --format '{{.Repository}}:{{.Tag}}' | grep -Pq "^${tag}\$" && [ "$do_ulak" == "yes" ]; then
+    if $docker images --format '{{.Repository}}:{{.Tag}}' | grep -Pq "^${tag}\$" && [ "$do_ulak" == "yes" ]; then
         echo -e "Image $tag already exists. Tagging '$tag' -> '$tag_ulak'\n"
-        docker image tag "$tag" "$tag_ulak"
+        $docker image tag "$tag" "$tag_ulak"
         return
     fi
 
@@ -31,15 +36,14 @@ build() {
     [[ -f "$dockerfile" ]] || { echo "Could not found $dockerfile"; exit 1; }
 
     echo -e "Building $tag\n"
-    docker buildx build --file $dockerfile --tag $tag $ver
+    $docker buildx build --file $dockerfile --tag $tag $ver
+
+    [[ "$do_ulak" == 'yes' ]] && $docker tag $tag $tag_ulak
 }
 
 build_all() {
     for key in "${!versions[@]}"; do
-        build $key no
-        echo
-
-        build $key yes
+        build $key $do_dev
         echo
     done
 }
@@ -51,29 +55,27 @@ push() {
     [[ "$2" == "yes" ]] && tag="$tag-dev"
 
     echo -e "Pushing $tag\n"
-    docker push $tag
+    $docker push $tag
 }
 
 push_all() {
     for key in "${!versions[@]}"; do
-        push $key no
-        echo
-
-        push $key yes
+        push $key $do_dev
         echo
     done
 }
 
 function usage() {
     echo "Usage:"
-    echo "  $0 [OPTIONS]"
+    echo "  $0 [OPTIONS...]"
     echo ""
     echo "Options:"
-    echo "  -d | --dev | --developer    Build/push developer versions"
-    echo "  -v | --ver | --version      Ubuntu version. One of 16.04, 18.04, 20.04, 22.04"
-    echo "  -p | --push                 Push built images"
-    echo "  -P | --push-only            Push images without building first"
-    echo "  -h | --help                 Show this help"
+    echo "  -d | --dev          Build/push developer versions"
+    echo "  -v | --ver          Ubuntu version. One of 16.04, 18.04, 20.04, 22.04"
+    echo "  -p | --push         Push built images"
+    echo "  -P | --push-only    Push images without building first"
+    echo "  -u | --ulak         Push images to Ulak's repos"
+    echo "  -h | --help         Show this help"
 }
 
 version=""
@@ -82,15 +84,19 @@ do_push="no"
 do_push_only="no"
 do_ulak="no"
 
-while [[ $# -gt 0 ]]; do
+args="$(getopt -o dpPuv:h, -l dev,push,push-only,ulak,ver:,help --name "$0" -- "$@")"
+eval set -- "$args"
+
+while true; do
     case "$1" in
         -d|--dev|--developer) do_dev="yes"; shift; ;;
         -v|--ver|--version) version="$2"; shift 2; ;;
         -p|--push) do_push="yes"; shift; ;;
         -P|--push-only) do_push_only="yes"; shift; ;;
         -u|--ulak) do_ulak="yes"; shift; ;;
-        -h|--help) usage; exit; ;;
-        *) shift; ;;
+        -h|--help) usage; exit 0; ;;
+        --) shift; break; ;;
+        *) echo "Unknown option $1"; exit 1; ;;
     esac
 done
 
